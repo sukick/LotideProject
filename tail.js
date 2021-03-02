b5ef7b37aba9adf766c3158675e84a78156d4efe
vagrant <suki.ckchan@gmail.com>
2021-03-02 07:15:33 +0000

    tail.js

--- tail.js
+++ tail.js
@@ -9,8 +9,12 @@ const assertEqual = function(actual, expected) {
 
 const tail = function(array) {
   let newArray = [];
-}
-// Test Case: Check the original array 
+  for (let i = 1; i <= array.length - 1; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
