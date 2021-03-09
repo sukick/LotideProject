676f164e57bc5db7fd640b9463152203b233d8a3
vagrant <suki.ckchan@gmail.com>
2021-03-09 07:54:30 +0000

    middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,16 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 const eqArrays = require('../eqArrays');
 
-assertArraysEqual(middle([1, 2, 3, 4]), [1, 2, 3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+describe("#middle", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });  
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
+
+// const assertArraysEqual = require('../assertArraysEqual');
+// assertArraysEqual(middle([1, 2, 3, 4]), [1, 2, 3, 4]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
