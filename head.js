b667f250499c1f1fb537f703bf7c4183805b984b
vagrant <suki.ckchan@gmail.com>
2021-03-09 05:14:37 +0000

    head.js

--- head.js
+++ head.js
@@ -1,18 +1,5 @@
-const assertEqual = function(actual, expected) {
- 
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}!== ${expected}`);
-  }
-};
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 3);
-assertEqual("Happy", "Happy");
-
 const head = function(array) {
   return array [0];
 }
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
