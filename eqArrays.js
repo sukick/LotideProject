275ea923489289d844416830f064d4895a2fc096
vagrant <suki.ckchan@gmail.com>
2021-03-09 05:43:13 +0000

    eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}!== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1 === null || arr2 === null) return false;
   if (arr1.length !== arr2.length) return false;
@@ -14,4 +6,6 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+module.exports = eqArrays;
+
