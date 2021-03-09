c9e6ba782a2f1e4f9d1ab95940f82116191176a0
vagrant <suki.ckchan@gmail.com>
2021-03-09 05:14:10 +0000

    assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,5 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual}!== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 3);
-assertEqual("Happy", "Happy");
+
+module.exports = assertEqual;
